612fe8b741e8362b80be43aad8664a216f9ebeac
marathon54 <ryan.consulting.group@gmail.com>
2015-10-13 19:41:04 -0400

    Oct13 Commit
    Updates

---
+++ script1.R
@@ -0,0 +1,24 @@
+# load the package
+library(mda)
+data(iris)
+# fit model
+fit <- mda(Species~., data=iris)
+# summarize the fit
+summary(fit)
+# make predictions
+predictions <- predict(fit, iris[,1:4])
+# summarize accuracy
+table(predictions, iris$Species)
+
+library(caret)
+data(iris)
+# fit model
+fit <- knn3(Species~., data=iris, k=5)
+# summarize the fit
+summary(fit)
+# make predictions
+predictions <- predict(fit, iris[,1:4], type="class")
+# summarize accuracy
+table(predictions, iris$Species)
+
+
